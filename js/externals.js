5d664043696f6ed0809a47a42766f5bf66c7f5ad
Joshua Melville <joshmelville@gmail.com>
2014-04-17 14:52:42 +0100

    Much more functionality

--- js/externals.js
+++ js/externals.js
@@ -1,6 +1,7 @@
 // CodeKit Imports
 
 // @codekit-prepend "../bower_components/jquery/dist/jquery.min.js"
+// @codekit-prepend "../bower_components/jquery-ui/ui/jquery-ui.js"
 // @codekit-prepend "../bower_components/bootstrap/dist/js/bootstrap.min.js"
 // @codekit-prepend "../bower_components/jquery.transit/jquery.transit.js"
 // @codekit-prepend "../bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js"
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var Transcriber = function (options) {
+
+	// Globals
 	var app = this;
 	var init = false;
 	var audioPlayer = document.getElementById("audio-player");
@@ -14,7 +16,11 @@ var Transcriber = function (options) {
 
 	var appOptions = options || defaultOptions;
 
+
+	// Helpers
+
 	function formatTime(seconds) {
+		// Format microseconds into minutes and seconds
 		minutes = Math.floor(seconds / 60);
 		minutes = (minutes >= 10) ? minutes : "0" + minutes;
 		seconds = Math.floor(seconds % 60);
@@ -23,10 +29,40 @@ var Transcriber = function (options) {
 	}
 	
 	function isNumber(n) {
+		// is n a number? return bool
  		return !isNaN(parseFloat(n)) && isFinite(n);
 	}
 
+	Array.contains = function (arr, key) {
+	    for (var i = arr.length; i--;) {
+	        if (arr[i] === key) return true;
+	    }
+	    return false;
+	};
 
+	Array.add = function (arr, key, value) {
+	    for (var i = arr.length; i--;) {
+	        if (arr[i] === key) return arr[key] = value;
+	    }
+	    this.push(key);
+	};
+
+	Array.prototype.remove = function(index) {
+	  var idx = index;
+	  if (idx != -1) {
+	      return this.splice(idx, 1); // The second parameter is the number of elements to remove.
+	  }
+	  return false;
+	}
+
+	Storage.prototype.setObject = function(key, obj) {
+	    return this.setItem(key, JSON.stringify(obj))
+	}
+	Storage.prototype.getObject = function(key) {
+	    return JSON.parse(this.getItem(key))
+	}
+
+    // Initialise app
 	this.init = function() {
 		$('#selectedFile').hide();
 		$('.open-music').on('click',function(){
@@ -60,11 +96,15 @@ var Transcriber = function (options) {
 				$('.participant-buttons').append('<button class="btn btn-primary btn-block individual">'+index+'</button>');
 				$('.radio').append('<label><input type="radio" name="participantRadio" value="'+index+'">'+index+'</label>');				
 		});
+		$('.participant-buttons').append('<div class="checkbox"><label><input type="checkbox" checked>Automatically switch</label></div>');
 		$('input:radio:first').prop("checked", true); 
 		$('.individual:first').addClass('active-individual');
 
-		// Offset the body with the footer height so no elements are unintentionally hidden behind it
+		$('.list-group').sortable({cancel: ':input,button,[contenteditable]', handle:'.drag-handle'});
+    	// $( ".list-group" ).disableSelection();
 
+
+		// Offset the body with the footer height so no elements are unintentionally hidden behind it
 		var footerHeight = $('footer').outerHeight();
 		var navHeight = $('nav').outerHeight()+15;
 		$('body').css({'marginBottom':footerHeight, 'marginTop':navHeight});
@@ -94,7 +134,7 @@ var Transcriber = function (options) {
 
 		$(document.body).on('click', '.list-group-item' ,function(){
 			var index = $(this).data('index');
-			var startTime = interviewData[index].startTime;
+			var startTime = interviewData[index] ? interviewData[index].startTime : 0;
 			app.seekTo(startTime);
 
 		});
@@ -116,15 +156,33 @@ var Transcriber = function (options) {
 		$(document.body).on('keypress', '.utterance' ,function(){
 		    if (event.which == 13) {
 		        event.preventDefault();
-
-		        app.updateUtterance($(this));
+		        app.updateUtterance($(this).parent().data('index'));
 		        $('.current-utterance').focus();
-		        var element = $(this);
-		        $(element).parent().transition({background:'rgb(232, 255, 235)'}, 800, 'ease');
-				timeout = setTimeout(function(){
-    				$(element).parent().transition({ background:'#ffffff'}, 1800, 'ease');
-					}, 1500);
-		    	}
+		    }
+		});
+
+		// Update existing utterance when enter key pressed
+		$(document.body).on('click', '.participant-heading' ,function(){
+	        if ($(this).html() == appOptions.participants[0]) {
+	        	$(this).html(appOptions.participants[1]);
+	        } else {
+	        	$(this).html(appOptions.participants[0]);
+	        }
+	        app.updateUtterance($(this).parent().data('index'));
+	        $('.current-utterance').focus();
+		});
+
+		// Remove utterance
+		$(document.body).on('click', '.remove-utterance' ,function(){
+	        app.removeUtterance($(this).parent().data('index'));
+	        $('.current-utterance').focus();
+		});
+
+		$(document.body).on('mouseenter', '.list-group-item' ,function(){
+			$(this).children('.remove-utterance').stop().transition({ opacity: 1}, 800, 'ease');
+		});
+		$(document.body).on('mouseleave', '.list-group-item' ,function(){
+			$(this).children('.remove-utterance').stop().transition({ opacity: 0}, 500, 'ease');
 		});
 
 		// Submit the form by pressing enter inside the main textarea
@@ -157,9 +215,48 @@ var Transcriber = function (options) {
 			app.toggleParticipant();   
     	});
 
+		app.loadData();
 		init = true;
 
 	};
+
+	this.saveData = function() {
+		console.log("Saving.");
+		var data = app.getInterviewData();
+		// console.log(data);
+		localStorage.setObject('data', data); 
+	};
+
+	this.setData = function(data) {
+		interviewData = data;
+	}
+
+	this.loadData = function() {
+		loadedData = localStorage.getObject('data') || {};
+		app.resetInterview();
+		 $.each(loadedData, function (index, value) {
+		 	var properties = {};
+			properties.participant = {};
+			properties.participant = loadedData[index].participant;
+			properties.utterance = {};
+			properties.utterance = loadedData[index].utterance;
+			properties.startTime = {};
+			properties.startTime = loadedData[index].startTime;
+			properties.submitTime = {};
+			properties.submitTime = loadedData[index].submitTime;
+            app.addUtterance(properties);
+            // console.log(properties);
+        });
+	};
+
+	this.downloadData = function() {
+		var filename = 'interview.json';
+		var text = JSON.stringify(app.getInterviewData(), undefined, 2); // indentation level = 2;
+	    var pom = document.createElement('a');
+	    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	    pom.setAttribute('download', filename);
+	    pom.click();		
+	}
 
 	this.toggleParticipant = function() {
 		$('input[type="radio"]').not(':checked').prop("checked", true);
@@ -179,32 +276,45 @@ var Transcriber = function (options) {
 		$('.time').html('00:00/00:00');
 		$(".list-group").empty();
 		$('.current-utterance').focus();
+		app.saveData();
 
 	}
 
 	this.addUtterance = function(data) {
 		interviewData.push(data);
 		var counter = interviewData.length-1;
-		var newItem = $('<a class="list-group-item" style="opacity:0;position:relative;top:200px" data-index="'+counter+'">').html('<h4 class="list-group-item-heading">'+data.participant+' <small>'+formatTime(data.startTime)+'-'+formatTime(data.submitTime)+'</small></h4><div contenteditable class="utterance">'+data.utterance+'</div></a>');
+		var newItem = $('<a class="list-group-item" style="opacity:0;position:relative;top:200px" data-index="'+counter+'">').html('<div class="drag-handle" style="background:#eee;height:45px;width:15px;float:left;margin-right:10px;cursor:move"></div><h4 class="list-group-item-heading participant-heading">'+data.participant+'</h4> <h5 contenteditable class="utterance-time">'+formatTime(data.startTime)+'-'+formatTime(data.submitTime)+'</h5><button type="button" class="btn btn-link btn-lg remove-utterance"><span class="glyphicon glyphicon-remove-circle"></span></button><div contenteditable class="utterance">'+data.utterance+'</div></a>');
 		$('.list-group').append(newItem);
 		newItem.transition({ opacity: 1,top:0,background:'rgb(255, 249, 182)'}, 800, 'ease');
 		$('body, html').stop().animate({ scrollTop: newItem.offset().top },1500);
 		timeout = setTimeout(function(){
     		newItem.transition({ background:'#fff'}, 1800, 'ease');
 		}, 1500);
+		app.saveData();
 	};
 
-	this.updateUtterance = function(element) {
-		interviewData[$(element).parent().data('index')].utterance = $(element).html();
-
+	this.updateUtterance = function(index) {
+		var element = $('*[data-index="'+index+'"]')
+		interviewData[index].utterance = $(element).children('.utterance').html();
+		interviewData[index].participant = $(element).children('.participant-heading').html();
+		app.saveData();
+        $(element).transition({background:'rgb(232, 255, 235)'}, 800, 'ease');
+		timeout = setTimeout(function(){
+			$(element).transition({ background:'#ffffff'}, 1800, 'ease');
+			}, 1500);
 	};
 
-	this.removeUtterance = function(data) {
+	this.removeUtterance = function(index) {
+		var element = $('*[data-index="'+index+'"]');
+		interviewData.remove(index);
+		timeout = setTimeout(function(){
+			$(element).transition({background:'Tomato',opacity: 0 }, function () { $(this).remove(); });
+			}, 500);
+		app.saveData();
 
 	};
 
 	this.loadAudio = function(files) {
-		app.resetInterview();
         var file = (window.webkitURL ? webkitURL : URL).createObjectURL(files[0]); 
         audioPlayer.src = file;
         audioPlayer.load();
@@ -236,7 +346,6 @@ var Transcriber = function (options) {
 	this.Stop = function() {
 		console.log('stopping');
 		audioPlayer.pause();
-		audioPlayer.currentTime = 0;
 		playing = false;
 	};
 
@@ -255,7 +364,12 @@ var Transcriber = function (options) {
 	};
 
 	this.seekTo = function(time) {
-		audioPlayer.currentTime = time;
+		if (audioPlayer.readyState > 2) {
+			audioPlayer.currentTime = time;			
+		}
+
+		startTime = currentTime || time;
+
 	}
 
 	app.init();
